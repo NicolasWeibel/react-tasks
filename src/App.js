5e1cd7a412374157dcce86490942ff12a5edeb04
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-10-03 16:59:45 -0300

    Add App structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import logo from "./images/logo-tasks.png";
 
 function App() {
-  return <div className="App">React</div>;
+  return (
+    <div className="App">
+      <div className="container-logo-tasks">
+        <img src={logo} className="logo-tasks" alt="Logo" />
+      </div>
+      <div className="tasks-main-list">
+        <h1>My tasks</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
